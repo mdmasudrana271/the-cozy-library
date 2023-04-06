2db090bd906dfff950b3387458aa0036f80b57a1
Md Abdullah Al Masud <126868744+abdullah-almasud@users.noreply.github.com>
2023-04-06 15:26:17 +0600

    change banner height and footer grid

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,12 +6,12 @@ import AnimatedText from "react-animated-text-content";
 
 const Banner = () => {
   return (
-    <section className="dark:bg-gray-800 dark:text-gray-100">
+    <section className="dark:bg-gray-800 dark:text-gray-100 h-[100vh]">
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <h1 className="text-3xl font-bold leading-none sm:text-6xl mr-1">
             We buy
-            <span className="text-orange-400 ml-1">
+            <span className="text-orange-400 ml-4">
               <AnimatedText
                 type="words" // animate words or chars
                 animation={{
@@ -29,9 +29,8 @@ const Banner = () => {
                 includeWhiteSpaces
                 threshold={0.1}
                 rootMargin="20%"
-                // loop={true}
               >
-                book
+                 book
               </AnimatedText>
             </span>
           </h1>
@@ -47,7 +46,6 @@ const Banner = () => {
           </p>
         </div>
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 ">
-          {/* <img src={book1} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" /> */}
           <Lottie className="md:w-72" animationData={animation} loop={true} />
         </div>
       </div>
@@ -57,4 +55,3 @@ const Banner = () => {
 
 export default Banner;
 
-// <h1 className="text-5xl font-bold">We buy book </h1>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
             organizations in Bangladesh
           </p>
         </div>
-        <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+        <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-3">
           <div className="space-y-3">
             <h3 className="tracking-wide uppercase dark:text-black md:text-xl font-medium">
               Company
